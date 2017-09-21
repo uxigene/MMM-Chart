4f51adfe64e4f73511cc541c1f0087455c5e8392
Marinescu Evghenii <marinescuEvghenii@gmail.com>
2017-09-21 02:06:36 +0200

    first commit

---
+++ MMM-Chart.js
@@ -0,0 +1,44 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Chart
+ *
+ * By Evghenii Marinescu https://github.com/MarinescuEvghenii/
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Chart", {
+    defaults: {
+        width       : 200,
+        height      : 200,
+        chartConfig : {}
+    },
+
+    getScripts: function() {
+		return ["modules/" + this.name + "/node_modules/chart.js/dist/Chart.bundle.min.js"];
+	},
+
+	start: function() {
+        this.config = Object.assign({}, this.defaults, this.config);
+		Log.info("Starting module: " + this.name);
+	},
+
+	getDom: function() {
+        // Create wrapper element
+        const wrapperEl = document.createElement("div");
+        wrapperEl.setAttribute("style", "position: relative; display: inline-block;");
+
+        // Create chart canvas
+        const chartEl  = document.createElement("canvas");
+        chartEl.width  = this.config.width;
+        chartEl.height = this.config.height;
+
+        // Init chart.js
+        this.chart = new Chart(chartEl.getContext("2d"), this.config.chartConfig);
+
+        // Append chart
+        wrapperEl.appendChild(chartEl);
+
+		return wrapperEl;
+	}
+});
---
+++ README.md
@@ -0,0 +1,43 @@
+# MMM-Chart
+Chart module for [MagicMirror²](https://github.com/MichMich/MagicMirror). This is a simple wrapper for [Chart.js](http://www.chartjs.org/)
+
+## Screenshot
+![](.github/example.png)
+
+## Installation
+
+In your terminal, go to your MagicMirror's Module folder:
+````
+cd ~/MagicMirror/modules
+````
+
+Clone this repository:
+````
+git clone git@github.com:MarinescuEvghenii/MMM-Chart.git
+````
+
+Configure the module in your `config.js` file.
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+	{
+		module: 'MMM-Chart',
+		config: {
+			// See 'Configuration options' for more information.
+		}
+	}
+]
+````
+
+## Configuration options
+
+The following property can be configured:
+
+| Option      | Description             | Default value |
+| ------------|-------------------------|:-------------:|
+| width       | Chart width in px.      | ```200```     |
+| height      | Chart height in px.     | ```200```     |
+| chartConfig | Chart.js config. More info at [chartjs.org](http://www.chartjs.org/) | ```{}``` |
